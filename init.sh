7c9cfb53196988779e0cd4b07c3c18f87f121bd4
Stephen Hong <mrsakkaro@dream-farmer.com>
2024-07-25 17:59:48 +0900

    make ports configurable

--- init.sh
+++ init.sh
@@ -8,4 +8,4 @@
 mkdir -p /root/derper/$TAILSCALE_DERP_HOSTNAME
 
 #Start Tailscale derp server
-/root/go/bin/derper --hostname $TAILSCALE_DERP_HOSTNAME --bootstrap-dns-names $TAILSCALE_DERP_HOSTNAME -certmode $TAILSCALE_DERP_CERTMODE -certdir /root/derper/$TAILSCALE_DERP_HOSTNAME --stun --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
+/root/go/bin/derper --hostname $TAILSCALE_DERP_HOSTNAME --bootstrap-dns-names $TAILSCALE_DERP_HOSTNAME -certmode $TAILSCALE_DERP_CERTMODE -certdir /root/derper/$TAILSCALE_DERP_HOSTNAME --a=$TAILSCALE_DERP_ADDRESS --stun --stun-port=$TAILSCALE_DERP_STUN_PORT --http-port=$TAILSCALE_DERP_HTTP_PORT --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
